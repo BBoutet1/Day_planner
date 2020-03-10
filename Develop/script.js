7a849b5148310cc6b9ec2dc54c61e152cc87be65
Brice Boutet <boutetlb@gmail.com>
2020-03-09 22:02:05 -0400

    current date and time using moment.js library

--- Develop/script.js
+++ Develop/script.js
@@ -3,18 +3,14 @@ var months = ["January","February","March","April","May","June","July","August",
 "September","October","November","December"]; // Months array
 var days = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"]; // day names array
 var d = new Date(); // date of the day
-var day = d.getDay();// current day of the week
-    day = days[day]; 
-var date = d.getDate();// current day date
-var month = d.getMonth(); // current month
-    month = months[month];
-var year = d.getFullYear(); // current year
 var hour =d.getHours(); //current hours
+console.log(hour)
 
 $(document).ready(function(){
 
     /** Current date display **/
-    $("#currentDay").text( day +" "+ date+" "+month+" "+year)
+    var today = moment().format('LLLL');
+    $("#currentDay").text( today)
 
     /** Generating hourly activities rows**/
     for (var i=9; i<17; i++){
@@ -43,7 +39,6 @@ $(document).ready(function(){
     /** 1 hour window start time */
     function windowStart(num){
 
-        console.log("numStart "+num)
         if (num<12)
         {
             timeStart = num+":00 AM";  //Morning
@@ -57,7 +52,6 @@ $(document).ready(function(){
             num=num-12;
             timeStart = num+":00 PM"; //afternoon
         }
-        console.log("numEnd "+num)
         return timeStart;
     }
 
