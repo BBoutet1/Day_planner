eba262bcc863cfbf5f0d7febb3421079540265dc
Brice Boutet <boutetlb@gmail.com>
2020-03-15 06:51:08 -0400

    Code clean-up and README

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,9 @@
-/*** Variables **/
-var hour = 11; //moment().hour();
 
 /** Current date display **/
-
+var hour = moment().hour(); // current hour
 function timeToday() {
     setInterval(function() {
-    
-        var today = moment().format('LL') + " "+moment().format('LTS') ;
+        var today = moment().format('LL') + " "+moment().format('LTS') ; // curret date and current time
         $("#currentDay").text( today)
     }, 1000);
 }
@@ -18,19 +15,19 @@ for (var i=9; i<17; i++){
     $(".container").append("<row id=\""+rowI+"\" class=\"row\">");
 }
 
-    /** Generating time, activity(time-bblock) and saving buttons**/
+    /** Generating time, activity(timebblock) and saving buttons**/
 for (i=9; i<17; i++){
-    var timeId = "time"+i; // 1 hour window label id
-    var blockId = "blockB"+i // window block id
+    var timeId = "time"+i; // 1 timeblock label id
+    var blockId = "blockB"+i // timeblock id
     var btnId = "B"+i; // window button Id
     $("#row"+i).append("<label id=\""+timeId+"\" class=\"hour col-2 text-right\">"); //time colum: id --> "time1", "time2", etc.
-    $("#row"+i).append("<textarea id=\""+blockId+"\" class=\"col-9\">"); //block column: id --> "blockB1", "blockB2", etc.
+    $("#row"+i).append("<textarea id=\""+blockId+"\" class=\"col-9\">"); //timeblock column: id --> "blockB1", "blockB2", etc.
     $("#row"+i).append("<button id=\""+btnId+"\" class=\"saveBtn col-1\">"); //button column: id --"B1", "B2", etc.
     }
 /** Button content : save icon  **/
 $(".saveBtn").append("<i class=\"fa fa-save\">");
 
-/** 1 hour window & block text */
+/** 1 timeblock */
 for (var j=9; j<17; j++){
     $("#time"+j).text(windowStart(j)+" - "+windowEnd(j));
 
@@ -38,7 +35,6 @@ for (var j=9; j<17; j++){
 
 /** 1 hour window start time */
 function windowStart(num){
-
     if (num<12)
     {
         timeStart = num+":00 AM";  //Morning
@@ -71,24 +67,23 @@ function windowEnd(num){
         num=num-12;
         timeEnd = num+":00 PM"; //Afternoon
     }
-
     return timeEnd;
 } 
 
     /** Time code color (background color) */
     for (var j=9; j<17; j++){
-    if (j<hour) { // passed activity blocks
+    if (j<hour) { // passed timeblocks
         $("#blockB"+j).addClass("past"); 
     }
-    else if(j==hour){ // current time block
+    else if(j==hour){ // present timeblock
         $("#blockB"+j).addClass("present"); 
     }  
-    else{ // upcomming time block
+    else{ // future timeblock
         $("#blockB"+j).addClass("future"); 
     }
 }
 
-/** Retrieving previously saved block text**/
+/** Retrieving previously saved timeblock**/
 for (var j=9; j<17; j++){
     var storageValue = localStorage.getItem("activityB"+j) // storage keys ---> "activityB1", "activityB2", etc.
     $("#blockB"+j).val(storageValue); // textarea Id ---> "blockB1", "blockB2"
--- README.md
+++ README.md
@@ -1,46 +1,41 @@
-# 05 Third-Party APIs: Work Day Scheduler
+# Work Day Scheduler
 
-Create a simple calendar application that allows the user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+A simple calendar application that allows the user to save events for each hour of the day. This app runs in the browser and features dynamically updated HTML and CSS powered by jQuery.
 
-You'll need to use the [Moment.js](https://momentjs.com/) library to work with date and time. Be sure to read the documentation carefully and concentrate on using Moment.js in the browser.
 
-## User Story
-
-```
-AS AN employee with a busy schedule
-I WANT to add important events to a daily planner
-SO THAT I can manage my time effectively
-```
-
-## Acceptance Criteria
+## Technologies used
 
 ```
-GIVEN I am using a daily planner to create a schedule
-WHEN I open the planner
-THEN the current day is displayed at the top of the calendar
-WHEN I scroll down
-THEN I am presented with timeblocks for standard business hours
-WHEN I view the timeblocks for that day
-THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-WHEN I click into a timeblock
-THEN I can enter an event
-WHEN I click the save button for that timeblock
-THEN the text for that event is saved in local storage
-WHEN I refresh the page
-THEN the saved events persist
+- HTML5
+- CSS3
+- Bootstrap
+- Javascript
+- jQuery
+- moment.js
 ```
 
-The following animation demonstrates the application functionality:
+## Functionalities
 
-![day planner demo](./Assets/05-third-party-apis-homework-demo.gif)
+```
+- Displays the current date and time when open 
+- Presents timeblocks with standard business hours (9am to 5pm)
+- 3 background colors to indicate whether it is in the past, present, or future
+- The timeblock is a multi-line inputs field to type your activities
+- Clicking the save button saves the timeblock in local storage
+- The saved event persists when the page is refreshed or reloaded
 
-## Review
+```
 
-You are required to submit the following for review:
+## Directions for future developements
 
-* The URL of the deployed application.
+```
+- Allow the user to select his own work hours
+- Allow the user to select desired timeblock length(1/2 and 1/4 an hour)
+- Handle the calendar reset
+- Handle browsers exceptions (local storage)
 
-* The URL of the GitHub repository. Give the repository a unique name and include a README describing the project.
+```
 
-- - -
-© 2019 Trilogy Education Services, a 2U, Inc. brand. All Rights Reserved.
+
+
+
